7089e07aa9d13bc184b7ac9ad736233d11414dd6
Bashar Nazarov <bashar.odilovich@gmail.com>
2023-02-07 13:47:19 +0500

    added Oauth config

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-
-const PORT = process.env.PORT || 5000
+require("./routes/authRoutes")(app);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
